c6ba61126713a9da90883a04020e3e169b7e594d
Radek Palisa <rpalisa@ebay.com>
2018-03-11 21:51:08 +0100

    swaping request for axios

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
+const axios = require('axios');
+const config = require('./config');
 
 const argv = yargs
     .options({
@@ -15,19 +15,33 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, result) => {
-    if (errorMessage) {
-        console.log(errorMessage);
-    } else {
-        //console.log(result.address);
+const encodedAddress = encodeURIComponent(argv.address);
+const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${config.GEO_API_KEY}`;
 
-        weather.getWeather(result.latitude,result.longitude, (errorMessage, weatherResults) => {
-            if (errorMessage) {
-                console.log(errorMessage);
-            } else {
-                console.log(`It's currently ${weatherResults.summary}, ${weatherResults.temperature}° in ${result.address}. It feels like ${weatherResults.apparentTemperature}°.`)
-            }    
-        });
-    }
-});
+axios.get(geocodeUrl)
+    .then(response => {      
+        if (response.data.status === 'ZERO_RESULTS') {
+            throw new Error('--- unable to find that address')
+        }
+        const lat = response.data.results[0].geometry.location.lat;
+        const lng = response.data.results[0].geometry.location.lng;
+        const weatherUrl = `https://api.darksky.net/forecast/${config.FORECAST_API_KEY}/${lat},${lng}?units=si&exclude=flags,daily,hourly`
+        console.log('--- ' + response.data.results[0].formatted_address + ' ---');
+        return axios.get(weatherUrl);
+    })
+    .then(response => {
+        const summary = response.data.currently.summary;
+        const temperature = response.data.currently.temperature;
+        const apparentTemperature = response.data.currently.apparentTemperature;
+
+        console.log(`It's currently ${summary}, ${temperature}°. It feels like ${apparentTemperature}°.`)
+    })
+    .catch(err => {
+        if (err.code === 'ENOTFOUND') {
+            console.log('--- unable to connect to API servers')
+        } else {
+            console.log(err.message)
+        }
+    })
+
 
